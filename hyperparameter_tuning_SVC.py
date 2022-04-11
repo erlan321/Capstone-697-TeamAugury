b46f38bba71bc5b2dd452c1dcb3f7ef1f6fc4838
Nywroc <a.wermenlinger@gmail.com>
2022-04-11 21:29:49 +0200

    LogReg and SVC hpt done

--- hyperparameter_tuning_SVC.py
+++ hyperparameter_tuning_SVC.py
@@ -2,21 +2,15 @@
 # All features will be used for the models.
 
 from inspect import classify_class_attrs
-import psycopg2
-from functions.Team_Augury_feature_functions import generate_features_csv
-import functions.Team_Augury_SQL_func
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.model_selection import GridSearchCV, StratifiedKFold, RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.ensemble import GradientBoostingClassifier
 import seaborn as sns
 import matplotlib.pyplot as plt
-import scipy.stats as stats
 
 import warnings
 warnings.filterwarnings('ignore')  ### BE CAREFUL USING THIS :) Supressing the warning that some LR are NaN
@@ -95,13 +89,13 @@ scoring = {'acc': 'accuracy', 'f1': 'f1'}
 
 # SHOULD CHANGE TO FUNCTION
 
-""" # Linear  
+# Linear  
 print("Initiating randomized grid search on linear SVC...")
-pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight="balanced", max_iter=-1, cache_size=2000, random_state=rnd_state))])
+pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight="balanced", max_iter=100000, cache_size=2000, random_state=rnd_state))])
 parameters = {"clf__C":np.logspace(-5, 15, num=21, base=2), 
             "clf__kernel":["linear"],
             'clf__gamma': np.logspace(-15, 3, num=19, base=2)}
-results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=2).fit(X, y)                                   
+results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=1).fit(X, y)                                   
 results = pd.DataFrame(results.cv_results_)
 results.columns = [col.replace('param_clf__', '') for col in results.columns]
 results.to_csv("saved_work/hp_tuning_RAW_SVC_linear.csv", index=False)
@@ -111,7 +105,7 @@ it_params_noC = ["gamma", "kernel"]
 tot_params = it_params + ["mean_train_acc", "mean_test_acc", "mean_train_f1", "mean_test_f1"]
 results = results[tot_params]
 results = results.melt(id_vars=it_params, var_name= "Score", value_name='Result')
-results["model_param"] =results["C"].round(5).astype(str) + " / " + results["gamma"].round(5).astype(str)
+results["model_param"] =results["C"].astype(str) + " / " + results["gamma"].astype(str)
 results['type'] = np.where(((results['Score']== 'mean_train_acc') | (results['Score']== 'mean_train_f1')), "train", "test")
 results['Score'] = np.where(((results['Score']== 'mean_train_acc') | (results['Score']== 'mean_test_acc')), "Accuracy", "F1")
 results = results[["Result", "model_param", "Score", "type", "C", "gamma", "kernel"]]
@@ -132,17 +126,17 @@ pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight=
 parameters = {"clf__C":np.logspace(-5, 15, num=21, base=2), 
             "clf__kernel":["rbf"],
             'clf__gamma': np.logspace(-15, 3, num=19, base=2)}
-results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=2).fit(X, y)                                   
+results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=1).fit(X, y)                                   
 results = pd.DataFrame(results.cv_results_)
 results.columns = [col.replace('param_clf__', '') for col in results.columns]
-results.to_csv("saved_work/hp_tuning_RAW_SVC_linear.csv", index=False)
+results.to_csv("saved_work/hp_tuning_RAW_SVC_rbf.csv", index=False)
 # clean up
 it_params = ["C", "gamma", "kernel"]
 it_params_noC = ["gamma", "kernel"]
 tot_params = it_params + ["mean_train_acc", "mean_test_acc", "mean_train_f1", "mean_test_f1"]
 results = results[tot_params]
 results = results.melt(id_vars=it_params, var_name= "Score", value_name='Result')
-results["model_param"] =results["C"].round(5).astype(str) + " / " + results["gamma"].round(5).astype(str)
+results["model_param"] =results["C"].astype(str) + " / " + results["gamma"].astype(str)
 results['type'] = np.where(((results['Score']== 'mean_train_acc') | (results['Score']== 'mean_train_f1')), "train", "test")
 results['Score'] = np.where(((results['Score']== 'mean_train_acc') | (results['Score']== 'mean_test_acc')), "Accuracy", "F1")
 results = results[["Result", "model_param", "Score", "type", "C", "gamma", "kernel"]]
@@ -157,6 +151,7 @@ results = results.drop(results[results.model_param.isin(remove_params)].index)
 #save
 results.to_csv("saved_work/hp_tuning_SVC_rbf.csv", index=False)
 
+
 # sigmoid  
 print("Initiating randomized grid search on sigmoid SVC...")
 pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight="balanced", max_iter=-1, cache_size=2000, random_state=rnd_state))])
@@ -164,10 +159,10 @@ parameters = {"clf__C":np.logspace(-5, 15, num=21, base=2),
             "clf__kernel":["sigmoid"],
             "clf__coef0": [0, 0.1, 1],
             'clf__gamma': np.logspace(-15, 3, num=19, base=2)}
-results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=2).fit(X, y)                                   
+results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=1).fit(X, y)                                   
 results = pd.DataFrame(results.cv_results_)
 results.columns = [col.replace('param_clf__', '') for col in results.columns]
-results.to_csv("saved_work/hp_tuning_RAW_SVC_linear.csv", index=False)
+results.to_csv("saved_work/hp_tuning_RAW_SVC_sigmoid.csv", index=False)
 # clean up
 it_params = ["C", "gamma", "kernel", "coef0"]
 it_params_noC = ["gamma", "kernel", "coef0"]
@@ -187,27 +182,27 @@ undr_fit = list(set(undr_fit['model_param']))
 remove_params = list(undr_fit + ovr_fit)
 results = results.drop(results[results.model_param.isin(remove_params)].index)
 #save
-results.to_csv("saved_work/hp_tuning_SVC_sigmoid.csv", index=False) """
+results.to_csv("saved_work/hp_tuning_SVC_sigmoid.csv", index=False) 
 
 # poly  
 print("Initiating randomized grid search on poly SVC...")
-pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight="balanced", max_iter=-1, cache_size=2000, random_state=rnd_state))])
+pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight="balanced", max_iter=100000, cache_size=2000, random_state=rnd_state))])
 parameters = {"clf__C":np.logspace(-5, 15, num=10, base=2), 
             "clf__kernel":["poly"],
             "clf__coef0": [0, 0.1, 1],
             "clf__degree": [3, 4, 5],
             'clf__gamma': np.logspace(-15, 3, num=10, base=2)}
-results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=2).fit(X, y)                                   
+results = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, scoring=scoring, refit="f1", n_jobs=-1, return_train_score=True, verbose=1).fit(X, y)                                   
 results = pd.DataFrame(results.cv_results_)
 results.columns = [col.replace('param_clf__', '') for col in results.columns]
-results.to_csv("saved_work/hp_tuning_RAW_SVC_linear.csv", index=False)
+results.to_csv("saved_work/hp_tuning_RAW_SVC_poly.csv", index=False)
 # clean up
 it_params = ["C", "gamma", "kernel", "coef0", "degree"]
 it_params_noC = ["gamma", "kernel", "coef0", "degree"]
 tot_params = it_params + ["mean_train_acc", "mean_test_acc", "mean_train_f1", "mean_test_f1"]
 results = results[tot_params]
 results = results.melt(id_vars=it_params, var_name= "Score", value_name='Result')
-results["model_param"] =results["C"].round(5).astype(str) + " / " + results["gamma"].round(5).astype(str) + " / " + results["coef0"].astype(str) + " / " + results["degree"].astype(str)
+results["model_param"] =results["C"].astype(str) + " / " + results["gamma"].astype(str) + " / " + results["coef0"].astype(str) + " / " + results["degree"].astype(str)
 results['type'] = np.where(((results['Score']== 'mean_train_acc') | (results['Score']== 'mean_train_f1')), "train", "test")
 results['Score'] = np.where(((results['Score']== 'mean_train_acc') | (results['Score']== 'mean_test_acc')), "Accuracy", "F1")
 results = results[["Result", "model_param", "Score", "type", "C", "gamma","coef0", "degree", "kernel"]]
