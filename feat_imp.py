900ba8c34e4e9472fdbd7a226af429ba7a8a4afe
qiopta <qiopta26@gmail.com>
2022-04-13 19:22:57 +0000

    shorter features WIP

--- feat_imp.py
+++ feat_imp.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 import pickle
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cla, figure
+import altair as alt
 
 
 X_train, y_train = load_and_preprocess()
@@ -45,7 +46,7 @@ clabels_v2 = clabels_v2[0:3] + clabels_v2[387:390] + list(GB_loaded['preprocesso
 #new_combined_ns = list(zip(clabels_v2,fimp))
 #print (f'first 40 {new_combined_ns[:40]}')
 
-# Plot feature importances
+# Plot feature importances 1 the long chart
 figure(figsize=(10, 80), dpi=100)
 
 def plot_feature_importance(model):
@@ -56,5 +57,40 @@ def plot_feature_importance(model):
   plt.ylabel("Feature")
   plt.ylim(-1, n_features)
 
-plot_feature_importance(GB_loaded.steps[1][1])
-plt.savefig("saved_work/feat_imp_v3.png")
+# plot_feature_importance(GB_loaded.steps[1][1])
+# plt.savefig("saved_work/feat_imp_v3.png")
+
+# Plot feature importances 2 focussed charts
+
+#step 1 aggreate the SBERT scores from the 805 features: 
+# the first 3 are originals
+# the next  3 are also originals
+# the next 31 are: 0-23 hours of day 0-6 day of week
+# Remainder are SBERT posts (384) and SBERT comments(384) 
+
+first_six_scores = fimp[0:6]
+hours_and_days = fimp[6:37]
+sbert_post = fimp[37:421]
+sbert_comment = fimp[421:806]
+
+#test lengths
+print ('Testing length of feature groups: should be 6,31,384,384.',len(first_six_scores), len(hours_and_days), len(sbert_post), len(sbert_comment))
+
+sbert_post    = np.array([np.sum(sbert_post)])
+sbert_comment = np.array([np.sum(sbert_comment)])
+
+#step 2 create new labels and array of scores
+scoring_array = np.concatenate(
+  (first_six_scores,
+  hours_and_days,
+  sbert_post,
+  sbert_comment),
+  axis=None
+)
+
+short_label_list = clabels_v2[0:37] + ['SBERT_posts', 'SBERT_comments']
+new_combined_ns = (zip(short_label_list,scoring_array))
+#print (new_combined_ns)
+
+df = pd.DataFrame(new_combined_ns, columns=['Feature','Importance'])
+print (df)
