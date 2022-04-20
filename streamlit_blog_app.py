33a4bfce14f7ffce23f1c902af1643a92ced67da
Erik <eriklang@umich.edu>
2022-04-19 21:29:57 -0400

    blog update

--- streamlit_blog_app.py
+++ streamlit_blog_app.py
@@ -527,7 +527,7 @@ if st.button("Test creating PRAW df for our pipeline"):
     st.write("predictions...", predictions)
     prediction_probas = clf.predict_proba(feature_df)
     st.write("prediction probabilities...", prediction_probas)
-    df = pd.concat([df, prediction_probas], axis=1)
+    df = pd.concat([df, prediction_probas], axis=0)
     st.write("output df",df)
 
 
