1b4e991c22e9db3ad38ddeb524131fc4a653370b
Nywroc <a.wermenlinger@gmail.com>
2022-04-20 11:32:45 +0200

    cleanup X_y and added RandomizedSearchCV

--- hyperparameter_tuning_SVC.py
+++ hyperparameter_tuning_SVC.py
@@ -1,4 +1,4 @@
-# This program was built to run an exhaustive GridSearchCV on the 2 selected models: LogisticRegression, SVC. GradientBoostingClassifier is tuned in the GBT specific file
+# This program was built to run an exhaustive GridSearchCV on the SVC model
 # All features will be used for the models.
 
 from inspect import classify_class_attrs
@@ -9,66 +9,17 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-
-import warnings
-warnings.filterwarnings('ignore')  ### BE CAREFUL USING THIS :) Supressing the warning that some LR are NaN
+from functions.Team_Augury_feature_functions import feature_to_x_y
 
 rnd_state = 42
 # This file should be generated from the baseline features csv file
 print("Loading features...")
 feature_df = pd.read_csv("saved_work/backup_features_data.csv")
 
-df = feature_df.copy()  #so code can be reused more easily
+X, y = feature_to_x_y(feature_df)
 
-# set target variable and calculate in df
-y_column = ["popular_hr_3"]
-popular_hr_3_threshold = 10
-
-df['hot_proxy_hr_3'] = df.apply(lambda x: x['upvotes_vs_hours'] if x['hours_since_created'] in [3,6,9,12,15,18,21,24] else np.nan, axis=1)
-df['hot_proxy_hr_3'] = df.groupby(['post_id'])['hot_proxy_hr_3'].fillna( method='backfill').fillna(value=0)
-df['hot_proxy_hr_3'] = df.apply(lambda x: np.nan if x['hours_since_created'] in [3,6,9,12,15,18,21,24] else x['hot_proxy_hr_3'], axis=1)
-df['hot_proxy_hr_3'] = df.groupby(['post_id'])['hot_proxy_hr_3'].fillna( method='backfill') .fillna(value=0) 
-df['popular_hr_3'] = df.apply( lambda x: 1 if x['hot_proxy_hr_3']>=popular_hr_3_threshold else 0, axis=1)
-
-
-
-### X - value decisions  ###
-
-### column names broken into categories for ease of use
-X_col_post_basic = ['post_author_karma', 'number_comments_vs_hrs']
-X_col_post_temporal = ['time_hour', 'day_of_week']
-X_col_comment_basic = ['avg_comment_upvotes_vs_hrs', 'avg_comment_author_karma']
-X_col_post_sentiment = ['post_sentiment']
-X_col_comment_sentiment = ['avg_comment_sentiment']
-X_col_post_sbert = [f'post_sbert_{"{:03d}".format(i)}' for i in range(1, 385)]
-X_col_comment_sbert = [f'avg_comment_sbert_{"{:03d}".format(i)}' for i in range(1, 385)]
-
-### columns that will need a standard scaler
 numeric_features = ['number_comments_vs_hrs','post_author_karma', 'avg_comment_upvotes_vs_hrs','avg_comment_author_karma']
-
-categorical_features = X_col_post_temporal
-
-
-### concat the X_value feature columns we want from the above categories
-X_columns = []
-X_columns.extend(X_col_post_basic)
-X_columns.extend(X_col_post_sentiment)
-X_columns.extend(X_col_post_temporal)
-X_columns.extend(X_col_post_sbert)
-X_columns.extend(X_col_comment_basic)
-X_columns.extend(X_col_comment_sentiment)
-X_columns.extend(X_col_comment_sbert)
-
-### increment selected by design = 0h
-df = df[df.hours_since_created==0]
-
-### Create table
-df = df.copy()[X_columns + y_column]
-X = df[X_columns]
-y = df[y_column].values.ravel()
-
-#print("lenght y: ", len(y)) #1674
-#print("sum y: ", sum(y)) #367
+categorical_features = ['time_hour', 'day_of_week']
 
 numeric_transformer = Pipeline(
     steps=[("scaler", StandardScaler())])
@@ -85,10 +36,8 @@ preprocessor = ColumnTransformer(
 #Scoring metrics
 scoring = {'acc': 'accuracy', 'f1': 'f1'}
 
-# SHOULD CHANGE TO FUNCTION
 
 # Linear  
-
 print("Initiating randomized grid search on linear SVC...")
 pipe = Pipeline(steps=[('preprocessor', preprocessor), ("clf", SVC(class_weight="balanced", max_iter=100000, cache_size=2000, random_state=rnd_state))])
 
