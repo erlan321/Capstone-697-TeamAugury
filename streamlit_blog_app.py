ac45f9942fdf86ec528f0d50ea8a42dce9fc64d2
qiopta <qiopta26@gmail.com>
2022-04-22 06:27:50 +0000

    Reference numbers in streamlit-blog-app updated

--- streamlit_blog_app.py
+++ streamlit_blog_app.py
@@ -64,52 +64,52 @@ st.markdown('''
 related_work = st.container()
 with related_work:
     related_work.info('''
-        **Title:** Predicting the Popularity of Reddit Posts with AI.[3]  
+        **Title:** Predicting the Popularity of Reddit Posts with AI.[2]  
         **Topic:** Supervised Learning approaches to predict popularity    
         **Implication for our project:** This is a prediction task on similar data.  The author uses Linear Regression, Random Forest Regression and a Neural Network to predict the number of upvotes. It ignores the temporal elements of Augury’s study and has a different approach to NLP using Bag of Words, TF-IDF (Term Frequency-Inverse Document Frequency), and LDA (Latent Dirichlet Allocation) trained on features extracted with Naive Bayes and SVM.  This work is based on regression, trying to predict the number of upvotes, whereas Augury aims to predict whether a post will be popular or not ( a classification problem) within a three hour window.   
         ''')
     related_work.info('''
-        **Title:** Data Stories. We analyzed 4 million data points to see what makes it to the front page of reddit. Here’s what we learned.[2]  
+        **Title:** Using Machine Learning to Predict the Popularity of Reddit Comments.[3]  
         **Topic:** Supervised Learning approaches to predicting Reddit comment Popularity    
         **Implication for our project:** Comparable aims, different NLP and looked only at Comments rather than posts.  Handling similar data, the author aimed to predict comment popularity.  Achieved relatively low accuracy scores ranging from 42% to 52.7% with a Decision Tree Classifier performing best. Cohen's Kappa statistic is applied to show results were, in fact, not much better than random.  In their conclusions they suggest research looks at temporal proximity of comments to posts, a key feature in Augury.
         ''')
     related_work.info('''
-        **Title:** Popularity prediction of reddit texts.[5]  
+        **Title:** Popularity prediction of reddit texts.[4]  
         **Topic:** Supervised learning approach to predict Reddit post popularity    
         **Implication for our project:** Comparable objectives, uses different NLP and features. Focuses on using Topics to determine predictive task.   Achieved 60-75% accuracy on the task, using Latent Dirichlet Allocation (LDA) and Term Frequency Inverse Document Frequency (TFIDF) to classify topics in posts to explore the relationship between topics and posts in order to predict using Naive Bayes and Support Vector Machine Classifiers what will become popular. Augury includes topic modeling as a feature, and our initial model suite included these classifiers.  
         ''')
     related_work.info('''
-        **Title:** Predicting the Popularity of Reddit Posts.[6]  
+        **Title:** Predicting the Popularity of Reddit Posts.[5]  
         **Topic:** Supervised learning approach to predict Reddit post popularity    
         **Implication for our project:** Conducted similar time of day, day of week features to Augury. Also performed sentiment analysis, with a different method. Finally treated the problem as a regression rather than classification one.  Our early experiments found classification to be better suited to our objective.
         ''')
     related_work.info('''
-        **Title:** Deepcas: An end-to-end predictor of information cascades.[7]  
+        **Title:** Deepcas: An end-to-end predictor of information cascades.[6]  
         **Topic:** Neural Network approach to predicting information cascades    
         **Implication for our project:** The prediction task in DeepCas was quite different to Augury. The problem definition included a Markov Decision Process as a ‘deep walk path’ making the work potentially relevant when we explored Reinforcement Learning approaches.  Eventually we moved away from these methods as our actor ‘choices’ i.e. picking a post had very little effect on the State/Environment hence we reject RL methods, despite a thorough investigation of use cases and an investigation of relevant works such as those in [8] to [11] below. The RL approach is effectively too contrived for our objective.
         ''')
     related_work.info('''
-        **Title:** Deep reinforcement learning with a combinatorial action space for predicting popular reddit threads.[8]  
+        **Title:** Deep reinforcement learning with a combinatorial action space for predicting popular reddit threads.[7]  
         **Topic:** Reinforcement Learning on Reddit data    
         **Implication for our project:** Similar domain space, different approaches.  Showed how a simulator might be used to reconstruct ‘Trees’ to set up and test sequential decision making. Related to our main task but not identical.
         ''')
     related_work.info('''
-        **Title:** Deep reinforcement learning with a natural language action space.[9]  
+        **Title:** Deep reinforcement learning with a natural language action space.[8]  
         **Topic:** Reinforcement Learning for NLP - Text based games    
         **Implication for our project:** Illustrated the large action space issue for deep Q-learning in NLP.  Helps understand why the Tree approach was taken in [8] in order to re-use approach from text based games when seeking to predict karma on reddit.  
         ''')
     related_work.info('''
-        **Title:** Deep reinforcement learning for NLP.[10]  
+        **Title:** Deep reinforcement learning for NLP.[9]  
         **Topic:** A primer on Reinforcement Learning for NLP    
         **Implication for our project:** Simple introduction and overview to papers in the domain.  
         ''')
     related_work.info('''
-        **Title:** SocialSift: Target Query Discovery on Online Social Media With Deep Reinforcement Learning.[11]  
+        **Title:** SocialSift: Target Query Discovery on Online Social Media With Deep Reinforcement Learning.[10]  
         **Topic:** Generation of SQL queries using Reinforcement Learning    
         **Implication for our project:**  Sets out an online method (via API) of testing created text (‘Queries’) where the returned results are classified to create a reward for the RL policy Pi. The text of the query is effectively keywords that are extracted from the corpus of the previous query history and returned results.
         ''')
     related_work.info('''
-        **Title:** Real-Time Predicting Bursting Hashtags on Twitter.[12]  
+        **Title:** Real-Time Predicting Bursting Hashtags on Twitter.[11]  
         **Topic:** Predicting hashtag bursts on Reddit    
         **Implication for our project:**  Similar data and has a temporal aspect to the prediction challenge. The definition of a ‘burst’ in this paper used a maximum function of hashtag counts over a 24 hour period to define a burst. Our early exploration of popularity as defined in Augury took inspiration from this, but was later adapted to our 3 hour target which better suited our objective. Augury also took some inspiration from the classification approach used in this paper.
         ''')
@@ -117,9 +117,9 @@ with related_work:
 st.subheader("") #create blank space
 st.subheader("Ethical Considerations")
 st.write('''
-    There are clearly ethical implications flowing from broadcasting messages on social media and the related investments to which these messages refer, as highlighted by the ‘Reddit Rally’ described above. In project Augury we are only looking at the popularity of posts, and we have not correlated this to market activity, which could be an extension of this work. To some extent this research has already been investigated by Muxi Xu  in 2021 [13] and also by Hu et al [14]. Therefore we have made no further mitigations in our project related to market ethics.  
+    There are clearly ethical implications flowing from broadcasting messages on social media and the related investments to which these messages refer, as highlighted by the ‘Reddit Rally’ described above. In project Augury we are only looking at the popularity of posts, and we have not correlated this to market activity, which could be an extension of this work. To some extent this research has already been investigated by Muxi Xu  in 2021 [12] and also by Hu et al [13]. Therefore we have made no further mitigations in our project related to market ethics.  
     
-    Social Media is often thought of as an open and public forum of discussion.  But an important ethical consideration of any data science project related to social media is that saying something in “public” may not necessarily mean “consent” to using a person’s name or username in published research [15].  While we did not have any need for usernames in the analysis we were performing, we did gather usernames into our database in order to track certain features of each Reddit post, and we are aware that if we were to ever use those usernames in future work, our database would could provide us with an already anonymized id number to protect each individuals’ privacy.  
+    Social Media is often thought of as an open and public forum of discussion.  But an important ethical consideration of any data science project related to social media is that saying something in “public” may not necessarily mean “consent” to using a person’s name or username in published research [14].  While we did not have any need for usernames in the analysis we were performing, we did gather usernames into our database in order to track certain features of each Reddit post, and we are aware that if we were to ever use those usernames in future work, our database would could provide us with an already anonymized id number to protect each individuals’ privacy.  
     
     A different aspect of  the free and open nature of social media, and Reddit in particular, is that it can  lead to environments which become toxic in a number of ways.  To ameliorate this we did take some action in our post-processing of our database.  The subreddits which are looking at Investment are typically more professional in nature so the main way in which toxicity occurs in these fora is through the use of profane language. Thus, in our pipeline we have removed this language from our analysis using the python module profanity-filter 1.3.3 which replaces profane words with the character “*”.  
 
@@ -247,7 +247,7 @@ with feature_table:
         st.markdown('''
             *Description:*  We used the SBERT library to encode the text of both Posts and Comments.  The SBERT interface was simple to use and produced in effect 380+ features for our classifiers for each post.  It provides a vectorial embedding of a sentence.  For the posts, this provides a vectorial space representation of each sentence that can be compared.  [(docs)](https://www.sbert.net/docs/pretrained_models.html)     
 
-            *Rationale:*  BERT produces state of the art word embeddings for many NLP tasks.  However, it is computationally intensive to ‘fine tune’ for specific tasks like classification and this will be especially slow for sentence similarity tasks.  For this reason we selected SBERT[19] which uses the BERT weights as a base model and creates fixed length embeddings for more efficient sentence comparison tasks.    
+            *Rationale:*  BERT produces state of the art word embeddings for many NLP tasks.  However, it is computationally intensive to ‘fine tune’ for specific tasks like classification and this will be especially slow for sentence similarity tasks.  For this reason we selected SBERT[15] which uses the BERT weights as a base model and creates fixed length embeddings for more efficient sentence comparison tasks.    
         ''')
     with feature_table.expander("Average Upvotes for the Top 5 Comments on the Post (per Hour)"):
         st.markdown('''
