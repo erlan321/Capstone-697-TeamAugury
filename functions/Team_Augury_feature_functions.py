85b84243064befe0a7c648a9036289ac0ca026c1
Erik <eriklang@umich.edu>
2022-04-22 05:46:30 -0400

    blog edit

--- functions/Team_Augury_feature_functions.py
+++ functions/Team_Augury_feature_functions.py
@@ -162,11 +162,11 @@ def comment_sentence_transform_func(df):
 
 def post_profanity_removal(df):
 
-    pf = ProfanityFilter() # set the filter
+    #pf = ProfanityFilter() # set the filter
 
-    # nlp = spacy.load('en_core_web_sm') #load spacy english language
-    # pf = ProfanityFilter(nlps={'en':nlp}) # set the filter
-    # nlp.add_pipe(pf.spacy_component, last=True)
+    nlp = spacy.load('en_core_web_sm') #load spacy english language
+    pf = ProfanityFilter(nlps={'en':nlp}) # set the filter
+    nlp.add_pipe(pf.spacy_component, last=True) #add to nlp pipeline per the documentation example
     
     def filter_profanity_func(text):
         return pf.censor(text)
